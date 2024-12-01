ddbce7ac713427d163fc249fbb1b7c5cc560fa7e
Alice Jacka <jacka.alice@gmail.com>
2024-12-01 11:05:14 +1100

    Add initial amCharts code

---
+++ src/lib/components/Chart.svelte
@@ -0,0 +1,83 @@
+<div class="chart" bind:this={chartdiv}></div>
+<style>
+.chart {
+    width: 100%;
+    height: 500px;
+}
+</style>
+<script lang="ts">
+import { onMount } from "svelte";
+import * as am5 from "@amcharts/amcharts5?client";
+import * as am5xy from "@amcharts/amcharts5/xy?client";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated?client";
+
+let chartdiv: HTMLDivElement;
+onMount(() => {
+    let root = am5.Root.new(chartdiv);
+    root.setThemes([am5themes_Animated.new(root)]);
+    let chart = root.container.children.push(
+      am5xy.XYChart.new(root, {
+        panY: false,
+        layout: root.verticalLayout
+      })
+    );
+    // Define data
+    let data = [{
+        category: "Research",
+        value1: 1000,
+        value2: 588
+      }, {
+        category: "Marketing",
+        value1: 1200,
+        value2: 1800
+      }, {
+        category: "Sales",
+        value1: 850,
+        value2: 1230
+      }
+    ];
+    // Create Y-axis
+    let yAxis = chart.yAxes.push(
+      am5xy.ValueAxis.new(root, {
+        renderer: am5xy.AxisRendererY.new(root, {})
+      })
+    );
+    // Create X-Axis
+    let xAxis = chart.xAxes.push(
+      am5xy.CategoryAxis.new(root, {
+        renderer: am5xy.AxisRendererX.new(root, {}),
+        categoryField: "category"
+      })
+    );
+    xAxis.data.setAll(data);
+    // Create series
+    let series1 = chart.series.push(
+      am5xy.ColumnSeries.new(root, {
+        name: "Series",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value1",
+        categoryXField: "category"
+      })
+    );
+    series1.data.setAll(data);
+    let series2 = chart.series.push(
+      am5xy.ColumnSeries.new(root, {
+        name: "Series",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "value2",
+        categoryXField: "category"
+      })
+    );
+    series2.data.setAll(data);
+    // Add legend
+    let legend = chart.children.push(am5.Legend.new(root, {}));
+    legend.data.setAll(chart.series.values);
+    // Add cursor
+    chart.set("cursor", am5xy.XYCursor.new(root, {}));
+    return () => {
+        root.dispose();
+    };
+});
+</script>
--- src/routes/task-2/+page.svelte
+++ src/routes/task-2/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import EngagementTable from "$lib/components/engagementTrendTable.svelte";
-import type { PageData } from "./$types";
+  import Chart from "$lib/components/Chart.svelte";
+  import type { PageData } from "./$types";
 
   let { data }: { data: PageData } = $props();
 </script>
 
+<div class="flex flex-col items-center justify-center">
+  <Chart data={data.data} />
+</div>
 <div class="flex flex-col"> <!-- items-center justify-center -->
   <EngagementTable artistTrends={data.data} />
 </div>
